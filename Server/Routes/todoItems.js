9be6012e3a6d9b02d58edcfd8fa532a92c02cec3
Natan Mendes <natamendes.gr062@academico.ifsul.edu.br>
2023-03-21 10:26:30 -0300

    ended backend

--- Server/Routes/todoItems.js
+++ Server/Routes/todoItems.js
@@ -1,20 +1,54 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 //importar model
-const todoItemsModel = require('../models/todoItems');
+const todoItemsModel = require("../models/todoItems");
 
 //rota para adicionar um item
+router.post("/api/item", async (req, res) => {
+  try {
+    const newItem = new todoItemsModel({
+      item: req.body.item,
+    });
 
-router.post('/api/items', async (req, res) => {
-    try {
-        const newItem = new todoItemsModel({
-            item: req.body.item
-        });
-        
-        //salvar item no mongodb
-        const savedItem = await newItem.save();
-        res.status(200).json('Item adicionado com sucesso!');
-    } catch (err) {
-        res.json(err);
-    }
+    //salvar item no mongodb
+    const saveItem = await newItem.save();
+    res.status(200).json("Item adicionado com sucesso!");
+  } catch (err) {
+    res.json(err);
+  }
 });
+
+//retornar data do banco de dados
+router.get("/api/items", async (req, res) => {
+  try {
+    const allTodoItems = await todoItemsModel.find({});
+    res.status(200).json(allTodoItems);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//atualizar um item
+router.put("/api/item/:id", async (req, res) => {
+  try {
+    //procurar item pelo id e atualizar
+    const updateItem = await todoItemsModel.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//deletar um item
+router.delete("/api/item/:id", async (req, res) => {
+  try {
+    //procurar item pelo id e deletar
+    const deleteItem = await todoItemsModel.findByIdAndDelete(req.params.id);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//exportar router
+module.exports = router;
--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
 //usar express.json() para poder conseguir a data no formato json
 app.use(express.json());
 
-//importar rotas
+//importar porta
 const PORT = process.env.PORT || 5500;
 
+//importar rotas
+const TodoItemRoute = require("./routes/todoItems");
+
 //conectar ao mongodb
-mongoose.connect(process.env.DB_CONNECT).then(() => console.log('Connected to DB!')).catch(err => console.log(err));
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(() => console.log("Connected to DB!"))
+  .catch((err) => console.log(err));
+
+//usar rotas
+app.use("/", TodoItemRoute);
 
 //adicionar porta e conectar ao mongodb
-app.listen( PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log("Servidor conectado"));
